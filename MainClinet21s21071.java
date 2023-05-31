728ef003e746bca2dcc91989479095eed12949ff
Maan2221 <135169208+Maan2221@users.noreply.github.com>
2023-05-31 23:58:58 +0400

    MainClinet21s21071.java

---
+++ MainClinet21s21071.java
@@ -0,0 +1,9 @@
+public class MainClinet21s21071
+{
+    public static void main(String args[])
+    {
+        Clinet21s21071 Maan_21S21071=new Clinet21s21071();
+        Thread t_Maan=new Thread(Maan_21S21071);
+        t_Maan.start();
+    }
+ }
